72fd72d418129cce9b54ae959cf2f5b93d12504e
Quentin PLA <quentin-pla@hotmail.fr>
2018-12-07 07:00:04 +0100

    Add files via upload

---
+++ index.php
@@ -0,0 +1,219 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <title>Diprozone</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css">
+    <link href="https://fonts.googleapis.com/css?family=Montserrat:300,400,500,600,700,800" rel="stylesheet">
+    <link rel="stylesheet" href="style.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"></script>
+</head>
+<body>
+    <div class="d-flex flex-column bg-dark justify-content-center">
+        <div class="d-flex justify-content-center align-items-center py-5 mb-4 bg-gradient1">
+            <img src="logo_n2i_color_moon.svg"/>
+            <img class="pr-4" src="logo_n2i_color_title.svg"/>
+            <h1 id="title" class="text-white text-center">Diprozone</h1>
+        </div>
+        <div id="membres" class="container-fluid bg-dark">
+            <h2 class="text-center text-white mb-4">Membres</h2>
+            <div class="row mb-3 px-5">
+                <div class="col d-flex flex-column">
+                    <img class="rounded-circle" src="greg.jpg"/>
+                    <h3>Grégory Nam</h3>
+                </div>
+                <div class="col d-flex flex-column">
+                    <img class="rounded-circle" src="xavier.jpg"/>
+                    <h3>Xavier Maisse</h3>
+                </div>
+                <div class="col d-flex flex-column">
+                    <img class="rounded-circle" src="thomas.jpg"/>
+                    <h3>Thomas Miceli</h3>
+                </div>
+                <div class="col d-flex flex-column">
+                    <img class="rounded-circle" src="https://i.pinimg.com/236x/d8/4c/d4/d84cd4a3c1c79cee7f6599414189f094--user-experience-avatar.jpg"/>
+                    <h3>Axel Amghar</h3>
+                </div>
+                <div class="col d-flex flex-column">
+                    <img class="rounded-circle" src="quentin.PNG"/>
+                    <h3>Quentin Pla</h3>
+                </div>
+                <div class="col d-flex flex-column">
+                    <img class="rounded-circle" src="louis.jpg"/>
+                    <h3>Louis Duttier</h3>
+                </div>
+            </div>
+        </div>
+        <div id="defis" class="container-fluid bg-light pt-4 pb-3">
+            <h2 class="text-center text-dark mb-4">Défis réalisés</h2>
+            <div class="card-columns mb-3 mx-5">
+                <a href="https://www.nuitdelinfo.com/inscription/defis/83">
+                    <div class="card rounded-3">
+                        <img class="card-img-top rounded-3" src="defi_logo.png" alt="Card image">
+                        <div class="card-body">
+                            <h4 class="card-title">C.LINE DESIGN</h4>
+                            <p class="card-text">Mascotte/Logo</p>
+                        </div>
+                    </div>
+                </a>
+                <a href="https://www.nuitdelinfo.com/inscription/defis/182">
+                    <div class="card rounded-3">
+                        <img class="card-img-top rounded-3" src="defi_radioking.png" alt="Card image">
+                        <div class="card-body">
+                            <h4 class="card-title">RADIO KING</h4>
+                            <p class="card-text">Réalisez le Jingle de la Radio de la Nuit !</p>
+                        </div>
+                    </div>
+                </a>
+                <a href="https://www.nuitdelinfo.com/inscription/defis/174">
+                    <div class="card rounded-3">
+                        <img class="card-img-top rounded-3" src="defi_nuit.png" alt="Card image">
+                        <div class="card-body">
+                            <h4 class="card-title">CRI</h4>
+                            <p class="card-text">Défi de la nuit 2018</p>
+                        </div>
+                    </div>
+                </a>
+                <a href="https://www.nuitdelinfo.com/inscription/defis/179">
+                    <div class="card rounded-3">
+                        <img class="card-img-top rounded-3" src="defi_feedapp.jpeg" alt="Card image">
+                        <div class="card-body">
+                            <h4 class="card-title">CCI LOZÈRE</h4>
+                            <p class="card-text">Feed the App !</p>
+                        </div>
+                    </div>
+                </a>
+            </div>
+        </div>
+        <div id="mascotte" class="container-fluid bg-dark py-5">
+            <div class="row align-items-center px-5 mb-5">
+                <div class="col-md-4 col-sm-12">
+                    <img class="d-block mx-auto" src="ch2019.PNG"/>
+                </div>
+                <div class="col-md-8 col-sm-12 px-5">
+                    <h2 class="text-white">Notre mascotte</h2>
+                    <p>Voici CH2019, un petit robot à la pointe de la technologie incluant de nombreux capteurs, le rendant très intelligent. Discret, efficace et curieux, il surveille son maître à la trace afin de le prévenir de tout danger à proximité. Ses deux caméras à grand angle permettent une vision globale de l'entourage. Lorsqu'il est surpris par un danger, la led positionnée sur son corps se met à clignoter en rouge et il émet un signal sonore puissant afin d'en avertir son maître. En ce moment c'est le cas, un cobra est en approche !</p>
+                </div>
+            </div>
+            <h2 class="text-center mb-5 text-white">Réalisation</h2>
+            <div id="mascotteapps" class="row align-items-center px-5">
+                <div class="col-12 mb-5">
+                    <div class="row align-items-center">
+                        <div class="col-md-2 col-sm-12">
+                            <img class="d-block ml-auto" src="assembly.PNG">
+                        </div>
+                        <div class="col-md-10 col-sm-12">
+                            <h4>Assembly</h4>
+                            <p>Création et positionnement des polygones.</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-12 mb-5">
+                    <div class="row align-items-center">
+                        <div class="col-md-2 col-sm-12">
+                            <img class="d-block ml-auto" src="phonto.PNG">
+                        </div>
+                        <div class="col-md-10 col-sm-12">
+                            <h4>Phonto</h4>
+                            <p>Création des textes CH2019 placé sur le robot et sur l'encadré.</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-12 mb-5">
+                    <div class="row align-items-center">
+                        <div class="col-md-2 col-sm-12">
+                            <img class="d-block ml-auto" src="skew.PNG">
+                        </div>
+                        <div class="col-md-10 col-sm-12">
+                            <h4>Skew</h4>
+                            <p>Inclinaison 3D du texte CH2019 afin d'obtenir un effet de perspective.</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-12 mb-5">
+                    <div class="row align-items-center">
+                        <div class="col-md-2 col-sm-12">
+                            <img class="d-block ml-auto" src="union.PNG">
+                        </div>
+                        <div class="col-md-10 col-sm-12">
+                            <h4>Union</h4>
+                            <p>Ajout d'effets sur les polygones.</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-12 mb-5">
+                    <div class="row align-items-center">
+                        <div class="col-md-2 col-sm-12">
+                            <img class="d-block ml-auto" src="pixelmator.PNG">
+                        </div>
+                        <div class="col-md-10 col-sm-12">
+                            <h4>Pixelmator</h4>
+                            <p>Superposition et ajout d'effets sur les différents polygones.</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <h3 class="text-center">(Nous avons seulement uilisé un iPhone 7 pour la réalisation complète de cette image)</h3>
+        </div>
+        <div id="assistvie" class="container-fluid bg-light pt-4 pb-3">
+            <h2 class="text-center mb-3 text-dark">Télé-Assistance Vie</h2>
+            <div class="row align-items-center mx-5">
+                <div class="col-12 mb-3">
+                    <h3>Choisissez un de nos profils proposés afin de découvrir son état de santé. Il est possible de consulter son activité, vigilance, conscience, fréquence cardiaque, dernier mouvement, et température corporelle.</h3>
+                </div>
+                <div class="col-12 mb-3">
+                    <h3>Les différentes données sont téléchargeables et accessibles sous forme de graphique.</h3>
+                </div>
+                <div class="col-12 mb-2">
+                    <img class="d-block mx-auto" src="diagassistdata.png"/>
+                </div>
+            </div>
+            <h3 class="text-center mb-3 text-dark">(Sur cet exemple, voici les données pour le patient Xavier Maisse)</h3>
+        </div>
+        <div id="blog" class="container-fluid bg-dark py-5">
+            <h2 class="text-center mb-4 text-white">Check-List, Planification, Suivi</h2>
+            <div class="row align-items-center mx-5">
+                <div class="col-12 mb-3">
+                    <h3 class="text-white">Disposition d'un blog permettant de renseigner toutes ses actions effectuées, un bilan sur sa santé afin d'obtenir des informations sur son sang et son urine et des informations sur l'état des engins utilisés.</h3>
+                </div>
+                <div class="col-12 mb-3">
+                    <h3 class="text-white">Il est également possible d'ajouter des tickets sur son blog afin de mettre à jour ses activités.</h3>
+                </div>
+            </div>
+        </div>
+        <div id="api" class="container-fluid bg-white py-5">
+            <h2 class="text-center mb-4 text-dark">Implémentation d'API</h2>
+            <div class="row align-items-center mx-5">
+                <div class="col-12 mb-3">
+                    <h3 class="text-dark">Obtenez des informations sur la météo en saisissant la ville souhaitée.</h3>
+                </div>
+                <div class="col-12 mb-3">
+                    <h3 class="text-dark">Obtenez des informations sur le trafic.</h3>
+                </div>
+                <div class="col-12 mb-3">
+                    <h3 class="text-dark">Obtenez des informations grâce à un flux RSS</h3>
+                </div>
+            </div>
+        </div>
+        <div id="cockpit" class="cfontainer-luid bg-dark py-5">
+            <h2 class="text-center mb-4 text-white">Cockpit</h2>
+            <div class="row align-items-center mx-5">
+                <div class="col-12 mb-3">
+                    <h3 class="text-white">Obtenez des informations cruciales pour le pilote.</h3>
+                </div>
+            </div>
+        </div>
+        <div id="musique" class="cfontainer-luid bg-white py-5">
+            <h2 class="text-center mb-4 text-dark">Gingle</h2>
+            <div class="row align-items-center mx-5">
+                <div class="col-12 mb-3">
+                    <h3 class="text-dark">Gingle pour la radio qui est diffusée durant la nuit de l'info.</h3>
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
